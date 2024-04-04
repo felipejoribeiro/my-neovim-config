04518c55bc447357d0fd3be7a5eedfd18f877fe4
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2024-04-04 10:07:10 -0300

    css update

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -1,21 +1,13 @@
 return {
   'jackMort/ChatGPT.nvim',
   event = 'VeryLazy',
+  config = function()
+    require('chatgpt').setup()
+  end,
   dependencies = {
     'MunifTanjim/nui.nvim',
     'nvim-lua/plenary.nvim',
     'folke/trouble.nvim',
     'nvim-telescope/telescope.nvim',
   },
-  config = function()
-    require('chatgpt').setup({
-      chat = {
-        question_sign = '👀',
-        answer_sign = '🤖',
-        keymaps = {
-          close = '<C-c>',
-        },
-      },
-    })
-  end,
 }
